a3b068022476af0ca598841e1a24359f691336f9
Saw Naw <waterchan@gmail.com>
2019-09-29 23:20:03 +0100

    Add project files.

---
+++ SampleVacancies.Data/SampleVacanciesDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SampleVacancies.Core.Models;
+using System;
+
+namespace SampleVacancies.Data
+{
+    public class SampleVacanciesDbContext : DbContext
+    {
+        public DbSet<Vacancy> Vacancies { get; set; }
+    }
+}
---
+++ SampleVacancies/Models/Vacancy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleVacancies.Core.Models
+{
+    public class Vacancy
+    {
+        public string Id { get; set; }
+        public string JobTitle { get; set; }
+        public string Location { get; set; }
+        public string Department { get; set; }
+        public string Salary { get; set; }
+        public string Description { get; set; }
+    }
+}
